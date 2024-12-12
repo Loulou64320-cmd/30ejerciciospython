e1e60e6f91e0fb97495d1fdd718b3ca6af16a611
Loulou64320-cmd <pedro.lainesortega64@gmail.com>
2024-12-12 06:57:47 -0500

    version01

---
+++ 04Adivina_numero01.py
@@ -0,0 +1,26 @@
+import random
+import os
+
+os.system('cls')
+
+print('"--- ADIVINA UN NUMERO GENERADO AL AZAR ---')
+
+print('\nAdivina numero entre 1 y 100')
+
+
+def numero_aleatorio(mi_numero):
+    numero_aleatorio = random.randint(1, 10)
+    return numero_aleatorio
+
+
+mi_numero = int(input('\tIngrese su numero : '))
+
+resultado = numero_aleatorio(mi_numero)
+
+
+if numero_aleatorio == mi_numero:
+    print('\t\tFelicitaciones ncontraste el numero ')
+    print(f'\t\tEl numero era {resultado}')
+else:
+    print('\t\tLo siento no lo encontraste ')
+    print(f'\t\tEl numero era {resultado}')
